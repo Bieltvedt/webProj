5f91f50d22ed50752755fa39db72198079a9c55b
dkosecioglu <dkosecioglu@gmail.com>
2023-01-20 14:35:22 +0100

    nothing really new. first push of assignment 2

---
+++ js/PartTwo.js
@@ -0,0 +1,10 @@
+
+var clickCount = 0;
+var mouseClicked = function() {
+  console.log('mouseClick = ' + clickCount);
+  clickCount++;
+}
+
+// ------------------------
+
+var buttonClick = document.querySelector

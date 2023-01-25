76a70bac942fe6b5d462bd84d1b4b51fe17d4a49
dkosecioglu <dkosecioglu@gmail.com>
2023-01-25 21:22:02 +0100

    me when

--- js-validation+part2/js/partOne.js
+++ js-validation+part2/js/partOne.js
@@ -2,7 +2,7 @@ window.onload = init;
 
 
 function init() {
-    initTwo();
+    // initTwo();
     console.log("test");
     const req = ["name", "email", "username", "password", "country", "zip", "sex", "language"];
     const signup = document.getElementById("signup-form");
@@ -56,14 +56,24 @@ function vpc(pc){       //postcode DOESNT WORK FOR SOME REASON?
         pc.style.color = "#FF6961";
         return false;
     }
-    if (val.charAt(4) != ('A'||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'R'||'S'||'T'||'Q'||'W'||'X'||'Z')) {
+    if (val.charAt(4) != 'A') {
         pc.style.color = "#FF6961";
         return false;
     }
-    if (val.charAt(5) != ('A'||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'R'||'S'||'T'||'Q'||'W'||'X'||'Z')) {
+    if (val.charAt(5) != 'A') {
         pc.style.color = "#FF6961";
         return false;
     }
+        
+        
+        
+    //     ||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'R'||'S'||'T'||'Q'||'W'||'X'||'Z')) {
+    //     pc.style.color = "#FF6961";
+    //     return false;
+    // }
+
+
+
     // if(/^[0-9]{4}[A-Z]{2}$/.test(val)){
     //     pc.style.color = "#645CAA";
     //     return true;
@@ -80,6 +90,10 @@ function vpw(pw) {      //password
     pw.style.color = "#FF6961";
 
     var us = false;
+
+    console.log("AAAAAAAAAAAAA");
+
+    if (val == null) return false;
 
     if ((val.includes('!') || val.includes('@') || val.includes('#') || val.includes('$') || val.includes('%') || val.includes('^') || val.includes('&') || val.includes('*') || val.includes('(') || val.includes(')') || val.includes('-') || val.includes('_') || val.includes('=') || val.includes('+') || val.includes('[') || val.includes(']') || val.includes('{') || val.includes('}') || val.includes(';') || val.includes(':') || val.includes('"') || val.includes('<') || val.includes(',') || val.includes('>') || val.includes('.') || val.includes('/') || val.includes('?') || val.includes('!')) && ( val.includes('1') ||  val.includes('2') ||  val.includes('3') ||  val.includes('4') ||  val.includes('5') ||  val.includes('6') ||  val.includes('7') ||  val.includes('8') ||  val.includes('9') ||  val.includes('0'))
     && (val.includes('A') || val.includes('B') || val.includes('C') || val.includes('D') || val.includes('E') || val.includes('F') || val.includes('G') || val.includes('H') || val.includes('I') || val.includes('J') || val.includes('K') || val.includes('L') || val.includes('M') || val.includes('N') || val.includes('O') || val.includes('P') || val.includes('R') || val.includes('S') || val.includes('T') || val.includes('U') || val.includes('V') || val.includes('Y') || val.includes('Z') || val.includes('X') || val.includes('Y') || val.includes('Z')) && (val.includes('a') || val.includes('b') || val.includes('c') || val.includes('d') || val.includes('e') || val.includes('f') || val.includes('g') || val.includes('h') || val.includes('i') || val.includes('j') || val.includes('k') || val.includes('l') || val.includes('m') || val.includes('n') || val.includes('o') || val.includes('p') || val.includes('r') || val.includes('s') || val.includes('t') || val.includes('u') || val.includes('v') || val.includes('y') || val.includes('z') || val.includes('x') || val.includes('y') || val.includes('z'))) {
@@ -149,10 +163,17 @@ function vem(em){       //email
 function vun(un){       //username
     let val = un.value;
 
-    if (val.charAt(0) != ('A'||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'R'||'S'||'T'||'Q'||'W'||'X'||'Z')) return false;
-    if (val.length <5 || val.length>12) return false; 
-    if ((val.charAt(val.length - 1)) != ('1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0')) return false;
+    if (val.charAt(0) != ('A'||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'R'||'S'||'T'||'Q'||'W'||'X'||'Z')) {
+        return false;
+    }
 
+    if (val.length <5 || val.length>12) {
+        return false; 
+    }
+
+    if ((val.charAt(val.length - 1)) != ('1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0')) {
+        return false;
+    }
 
     // if(val.length > 5 && val.length && /^[A-Z].*?([0-9]|[-._!`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|])$/.test(val)){
     //     pw.style.color = "#645CAA";

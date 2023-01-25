2706aa268942315ebfb4eb35ef5e14c72ce9c375
Bieltvedt <isakbieltvedt@gmail.com>
2023-01-25 15:58:04 +0100

    new stuff

--- js/PartTwo.js
+++ js/PartTwo.js
@@ -1,4 +1,11 @@
 
+window.onload = init;
+
+function init(){
+  buttons = document.querySelectorAll('button');
+  
+}
+
 var clickCount = 0;
 var mouseClicked = function() {
   console.log('mouseClick = ' + clickCount);

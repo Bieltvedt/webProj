9d461ffbd501eea3a1bb3deea127279bfacd5410
Bieltvedt <isakbieltvedt@gmail.com>
2023-01-25 22:42:35 +0100

    fixed vpc and some more stuff

--- js validation/js/partOne.js
+++ js validation/js/partOne.js
@@ -30,36 +30,40 @@ function init() {
 }
 
 
-function vpc(pc){       //postcode DOESNT WORK FOR SOME REASON? 
+function vpc(pc){       
     let val = pc.value;
+    let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    let chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'Q', 'W', 'X', 'Z']
 
     pc.style.color = "#645CAA";
 
     if (val.length != 6) return false;
-    if (val.charAt(0) != ('1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0')) {
-        pc.style.color = "#FF6961";
-        return false;
-    } 
-    if (val.charAt(1) != ('1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0')) { 
+    if (numbers.indexOf(val.charAt(0)) === -1) {
         pc.style.color = "#FF6961";
         return false;
     }
-    if (val.charAt(2) != ('1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0')) {
+    if (numbers.indexOf(val.charAt(1)) === -1) {
         pc.style.color = "#FF6961";
         return false;
     }
-    if (val.charAt(3) != ('1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0')) {
+    if (numbers.indexOf(val.charAt(2)) === -1) {
         pc.style.color = "#FF6961";
         return false;
     }
-    if (val.charAt(4) != ('A'||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'R'||'S'||'T'||'Q'||'W'||'X'||'Z')) {
+    if (numbers.indexOf(val.charAt(3)) === -1) {
         pc.style.color = "#FF6961";
         return false;
     }
-    if (val.charAt(5) != ('A'||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'R'||'S'||'T'||'Q'||'W'||'X'||'Z')) {
+    if (chars.indexOf(val.charAt(4)) === -1) {
         pc.style.color = "#FF6961";
         return false;
     }
+    if (chars.indexOf(val.charAt(5)) === -1) {
+        pc.style.color = "#FF6961";
+        return false;
+    }
+    
+    return true;
     // if(/^[0-9]{4}[A-Z]{2}$/.test(val)){
     //     pc.style.color = "#645CAA";
     //     return true;
@@ -73,14 +77,14 @@ function vpc(pc){       //postcode DOESNT WORK FOR SOME REASON?
 function vpw(pw) {      //password
     let val = pw.value;
 
-    pw.style.color = "#FF6961";
-
     var us = false;
 
     if ((val.includes('!') || val.includes('@') || val.includes('#') || val.includes('$') || val.includes('%') || val.includes('^') || val.includes('&') || val.includes('*') || val.includes('(') || val.includes(')') || val.includes('-') || val.includes('_') || val.includes('=') || val.includes('+') || val.includes('[') || val.includes(']') || val.includes('{') || val.includes('}') || val.includes(';') || val.includes(':') || val.includes('"') || val.includes('<') || val.includes(',') || val.includes('>') || val.includes('.') || val.includes('/') || val.includes('?') || val.includes('!')) && ( val.includes('1') ||  val.includes('2') ||  val.includes('3') ||  val.includes('4') ||  val.includes('5') ||  val.includes('6') ||  val.includes('7') ||  val.includes('8') ||  val.includes('9') ||  val.includes('0'))
     && (val.includes('A') || val.includes('B') || val.includes('C') || val.includes('D') || val.includes('E') || val.includes('F') || val.includes('G') || val.includes('H') || val.includes('I') || val.includes('J') || val.includes('K') || val.includes('L') || val.includes('M') || val.includes('N') || val.includes('O') || val.includes('P') || val.includes('R') || val.includes('S') || val.includes('T') || val.includes('U') || val.includes('V') || val.includes('Y') || val.includes('Z') || val.includes('X') || val.includes('Y') || val.includes('Z')) && (val.includes('a') || val.includes('b') || val.includes('c') || val.includes('d') || val.includes('e') || val.includes('f') || val.includes('g') || val.includes('h') || val.includes('i') || val.includes('j') || val.includes('k') || val.includes('l') || val.includes('m') || val.includes('n') || val.includes('o') || val.includes('p') || val.includes('r') || val.includes('s') || val.includes('t') || val.includes('u') || val.includes('v') || val.includes('y') || val.includes('z') || val.includes('x') || val.includes('y') || val.includes('z'))) {
         pw.style.color = "#645CAA";
         us = true;
+    }else {
+        pw.style.color = "#FF6961";
     }
 
     if (val.length < 14) {
@@ -103,15 +107,17 @@ function vpw(pw) {      //password
 function vnm(nm) {      //name
     let val = nm.value;
 
+
+    nm.style.color = "#645CAA";
     if (val.includes('!') || val.includes('@') || val.includes('#') || val.includes('$') || val.includes('%') || val.includes('^') || val.includes('&') || val.includes('*') || val.includes('(') || val.includes(')') || val.includes('-') || val.includes('_') || val.includes('=') || val.includes('+') || val.includes('[') || val.includes(']') || val.includes('{') || val.includes('}') || val.includes(';') || val.includes(':') || val.includes('"') || val.includes('<') || val.includes(',') || val.includes('>') || val.includes('.') || val.includes('/') || val.includes('?') || val.includes('!')) {
-        pc.style.color = "#FF6961";
+        nm.style.color = "#FF6961";
         return false;
     }
     if ( val.includes('1') ||  val.includes('2') ||  val.includes('3') ||  val.includes('4') ||  val.includes('5') ||  val.includes('6') ||  val.includes('7') ||  val.includes('8') ||  val.includes('9') ||  val.includes('0')) {
-        pc.style.color = "#FF6961";
+        nm.style.color = "#FF6961";
         return false; 
     }
-
+    return true;
     // if(/[A-z]+/.test(val)){
     //     pw.style.color = "#645CAA";
     //     return true;
@@ -133,6 +139,7 @@ function vem(em){       //email
 
     if (att < 2) return false;
 
+    return true;
     // if(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val)){
     //     pw.style.color = "#645CAA";
     //     return true;
@@ -148,7 +155,7 @@ function vun(un){       //username
     if (val.charAt(0) != ('A'||'B'||'C'||'D'||'E'||'F'||'G'||'H'||'I'||'J'||'K'||'L'||'M'||'N'||'O'||'P'||'R'||'S'||'T'||'Q'||'W'||'X'||'Z')) return false;
     if (val.length <5 || val.length>12) return false; 
     if ((val.charAt(val.length - 1)) != ('1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0')) return false;
-
+    return true;
 
     // if(val.length > 5 && val.length && /^[A-Z].*?([0-9]|[-._!`'#%&,:;<>=@{}~\$\(\)\*\+\/\\\?\[\]\^\|])$/.test(val)){
     //     pw.style.color = "#645CAA";
--- regex/js/PartTwo.js
+++ regex/js/PartTwo.js
@@ -1,9 +1,11 @@
+//initializing all our variables, and attaching to init to onload
 window.onload = init;
 let clickCount = 0;
 let charCount = 0;
 let kpCount = 0;
 let date;
 
+//attaches all event handleing functions to their respective elements. Also initializes our "clock"
 function init(){
   buttons = document.querySelectorAll('button');
   for(button of buttons) {
@@ -23,12 +25,13 @@ function init(){
   date = new Date();
 }
 
-
+//handles button click, increments counter
 let buttonClick = function() {
   console.log('mouseClick = ' + clickCount);
   clickCount++;
 }
 
+//handles keypress, increments keypress counter, if the key typed is a charecter -> icrement charcount
 let formKeyPress = (e) => {
   let char = String.fromCharCode(e.keyCode);
   console.log('keypress = ' + char);
@@ -38,38 +41,37 @@ let formKeyPress = (e) => {
   } 
 }
 
+//reveals the statistics for this "session"
 let revealStats = (e) => {
   e.preventDefault();
+  //submitting the form doesnt trigger buttonClick(), even though submitting is a "button"
   clickCount++;
 
 
+  //insert stats into hidden <p>'s in hidden div
   let hidden = document.querySelector(".hidden");
   let children = hidden.children;
   let test = new Date();
   let time = formatTime(test - date);
-
   children[0].innerHTML = "Num. of mouse clicks: " + clickCount;
   children[1].innerHTML = "Time on page: " + time;
   children[2].innerHTML = "Key presses: " + kpCount;
   children[3].innerHTML = "Chars typed: " + charCount;
 
   
-
-  // children[0].appendChild(document.createTextNode("Num. of mouse clicks: " + clickCount));
-  // children[1].appendChild(document.createTextNode("Time on page: " + time));
-  // children[2].appendChild(document.createTextNode("Key presses: " + kpCount));
-  // children[3].appendChild(document.createTextNode("Chars typed: " + charCount));
-
+  //unhide 
   hidden.style.display = "block";
   reset();
 }
 
+//converts milliseconds into human readable format
 let formatTime = (millis) => {
   let min = Math.floor(millis / 60000);
   let sec = ((millis % 60000) / 1000).toFixed(0);
   return min + ":" + sec;
 }
 
+//resets counters and "clock"
 let reset = () => {
   date = new Date();
   clickCount = 0;
